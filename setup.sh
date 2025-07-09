8296a50169862b452c2507f4382c9bb1b7dc157c
Yan Yan <yanyansmajesty@outlook.com>
2025-07-09 13:54:54 +0200

    Add vim plugin manager

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Download VIM plugin manger
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 cp .vimrc .gvimrc ${HOME}
 cp .indent.pro ${HOME}
 
-sudo usermod -aG vboxsf $(whoami)
+# Only for VirtualBox client
+# sudo usermod -aG vboxsf $(whoami)
